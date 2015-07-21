82d07450221e335a1afd1094410d52411ffef683
Loïc <loic.porte@corp.ovh.com>
2015-07-21 10:38:44 +0200

    Update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import platform
+import codecs
+import yaml
 
 from flask import Flask
 from flask_redis import Redis
@@ -6,17 +8,28 @@ from flask_redis import Redis
 app = Flask(__name__)
 app.config['REDIS_HOST'] = 'localhost'
 app.config['REDIS_DB'] = '5'
+app.config['REDIS_KEY'] = 'count'
 
+
+# Locate the config file to use
+config_file = 'config.yml'
+try:
+    # Open and read the config file
+    with codecs.open(config_file, 'r', 'utf8') as file_handler:
+        conf = yaml.load(file_handler)
+        app.config.update(conf)
+except Exception as e:
+    print 'Error: %s' % e
 redis = Redis(app)
 
 
 @app.route('/')
 def homepage():
-    count = int(redis.get('count'))
+    count = redis.get(app.config['REDIS_KEY'])
     if not count:
-        count = 0
-    redis.set('count', count + 1)
-    return "Hi, I'm %s, this page has been seen %d times." % (
+        count = 1
+    redis.set(app.config['REDIS_KEY'], int(count) + 1)
+    return "Hi, I'm %s, this page has been seen %s times." % (
         platform.node(), count)
 
 
